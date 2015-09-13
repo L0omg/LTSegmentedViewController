999c056cc2dc58ebc239fc0d1e0a5003dd6b7f14
LYP <714016913@qq.com>
2015-09-13 21:10:27 +0800

    Initial Commit

---
+++ LTSegmentedViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LTSegmentedViewController
+//
+//  Created by LYP on 15/9/13.
+//  Copyright (c) 2015年 LYP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LTSegmentedViewController/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LTSegmentedViewController
+//
+//  Created by LYP on 15/9/13.
+//  Copyright (c) 2015年 LYP. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
